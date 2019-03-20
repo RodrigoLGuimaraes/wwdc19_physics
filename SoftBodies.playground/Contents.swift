c6354341a65eef54193cecbf86aa4941aab47c09
Rodrigo Longhi Guimarães <rguimaraes@alunos.utfpr.edu.br>
2019-03-20 00:15:42 -0300

    Create launcher class

--- SoftBodies.playground/Contents.swift
+++ SoftBodies.playground/Contents.swift
@@ -1,11 +1,3 @@
 //: A SpriteKit based Playground
 
-import PlaygroundSupport
-import SpriteKit
-
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
-//sceneView.showsPhysics = true
-let scene = GameScene(size: sceneView.frame.size)
-scene.scaleMode = .aspectFill
-sceneView.presentScene(scene)
-PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
+Launcher.launch()
---
+++ SoftBodies.playground/Sources/Launcher.swift
@@ -0,0 +1,13 @@
+import PlaygroundSupport
+import SpriteKit
+
+public class Launcher {
+    public static func launch() {
+        let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+        //sceneView.showsPhysics = true
+        let scene = GameScene(size: sceneView.frame.size)
+        scene.scaleMode = .aspectFill
+        sceneView.presentScene(scene)
+        PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
+    }
+}
